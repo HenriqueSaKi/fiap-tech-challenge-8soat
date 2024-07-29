eb6d8c04bb9e53565739638e6a444ac5e2a02a92
felipeBarras <felipeu_santos@hotmail.com>
2024-07-29 17:18:35 -0300

    adicionando o AtomicInteger para criar o id dos pedidos

--- app/src/main/java/br/com/fiap/tech_challenge/domain/Pedido.java
+++ app/src/main/java/br/com/fiap/tech_challenge/domain/Pedido.java
@@ -8,9 +8,15 @@ import lombok.Data;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Data
 public class Pedido {
+
+    private static final AtomicInteger count = new AtomicInteger(0);
+
+    @Schema(name = "id", description = "Identificador único do pedido", example = "000001")
+    private int id;
 
     @Schema(name = "dataPedido", description = "Data em que o pedido foi atualizado")
     private Date dataPedido;
@@ -24,9 +30,28 @@ public class Pedido {
     @Schema(name = "itens", description = "Lista de itens do pedido")
     private List<ItemPedido> itens;
 
+    public Pedido(Date dataPedido, SituacaoPedido situacaoPedido, List<ItemPedido> itens) {
+        this.id = count.incrementAndGet();
+        this.dataPedido = dataPedido;
+        this.situacaoPedido = situacaoPedido;
+        this.itens = itens;
+        this.valorTotalPedido = getValorTotalPedido();
+    }
+
     public BigDecimal getValorTotalPedido() {
         return itens.stream()
                 .map(ItemPedido::getValorTotalItem)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{"+
+                "id=" + id +
+                ", dataPedido=" + dataPedido +
+                ", valorTotalPedido=" + valorTotalPedido +
+                ", situacaoPedido=" + situacaoPedido +
+                ", itens=" + itens +
+                "}";
     }
 }
