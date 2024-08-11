95478b4c9ceae3230b18e57f8d623abcfdbd4714
Henrique Kisaki <satoshikisaki@hotmail.com>
2024-08-10 23:11:39 -0300

    refactor: remoção das validações da classe ClienteController

--- src/main/java/br/com/fiap/tech_challenge/adapters/driver/controller/ClienteController.java
+++ src/main/java/br/com/fiap/tech_challenge/adapters/driver/controller/ClienteController.java
@@ -23,48 +23,26 @@ public class ClienteController implements ClienteSwaggerInterface {
     }
 
     @Override
-    public ResponseEntity<String> heathCheck() {
-        return new ResponseEntity<>("OK", HttpStatus.OK);
-    }
-
-    @Override
     public ResponseEntity<String> cadastrarCliente(@RequestBody ClienteDTO clienteDTO) {
         service.cadastrarCliente(clienteDTO);
         return new ResponseEntity<>("Cliente cadastrado com sucesso!", HttpStatus.CREATED);
     }
 
     @Override
-    public ResponseEntity<Object> buscarClientePorCPF(@PathVariable String cpf) {
+    public ResponseEntity<ClienteDTO> buscarClientePorCPF(@PathVariable String cpf) {
         ClienteDTO clienteDTO = service.buscarClientePorCPF(cpf);
-        if(clienteDTO != null) {
-            return new ResponseEntity<>(clienteDTO, HttpStatus.OK);
-        }
-        else {
-            return new ResponseEntity<>("Cliente não encontrado", HttpStatus.NOT_FOUND);
-        }
+        return new ResponseEntity<>(clienteDTO, HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<Object> atualizarCliente(@RequestBody ClienteDTO clienteDTO) {
-        try {
-            service.atualizarCliente(clienteDTO);
-            return new ResponseEntity<>(clienteDTO, HttpStatus.ACCEPTED);
-        }
-        catch (Exception e) {
-            return new ResponseEntity<>("Erro ao atualizar as informações do cliente.", HttpStatus.BAD_REQUEST);
-        }
-
+    public ResponseEntity<ClienteDTO> atualizarCliente(@RequestBody ClienteDTO clienteDTO) {
+        service.atualizarCliente(clienteDTO);
+        return new ResponseEntity<>(clienteDTO, HttpStatus.ACCEPTED);
     }
 
     @Override
     public ResponseEntity<String> excluirCliente (@PathVariable Long id) {
-        if(service.existeCliente(id)) {
-            service.excluirCliente(id);
-            return new ResponseEntity<>("Cliente excluído com sucesso!", HttpStatus.OK);
-        }
-        else {
-            return new ResponseEntity<>("Cliente não encontrado.", HttpStatus.NOT_FOUND);
-        }
+        return new ResponseEntity<>(service.excluirCliente(id), HttpStatus.OK);
     }
 
 }
