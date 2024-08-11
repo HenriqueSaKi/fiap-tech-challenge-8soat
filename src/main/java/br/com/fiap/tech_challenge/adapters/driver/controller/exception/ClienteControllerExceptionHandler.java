87a2842452394eed8247a4a031e7e9ae1c77bf48
Henrique Kisaki <satoshikisaki@hotmail.com>
2024-08-11 00:04:52 -0300

    refactor: repackage ClienteControllerExceptionHandler class

--- src/main/java/br/com/fiap/tech_challenge/adapters/driver/controller/exception/ClienteControllerExceptionHandler.java
+++ src/main/java/br/com/fiap/tech_challenge/adapters/driver/controller/exception/ClienteControllerExceptionHandler.java
@@ -1,4 +1,4 @@
-package br.com.fiap.tech_challenge.adapters.driver.controller;
+package br.com.fiap.tech_challenge.adapters.driver.controller.exception;
 
 import br.com.fiap.tech_challenge.core.application.exception.*;
 import org.springframework.http.HttpStatus;
