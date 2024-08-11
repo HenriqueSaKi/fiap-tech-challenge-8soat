658a1b209ae5d2736044fae40cd8d750e9421da6
Henrique Kisaki <satoshikisaki@hotmail.com>
2024-08-11 16:31:20 -0300

    feat: Implementação da service no ProdutoController

--- src/main/java/br/com/fiap/tech_challenge/adapters/driver/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/tech_challenge/adapters/driver/controller/ProdutoController.java
@@ -1,10 +1,50 @@
 package br.com.fiap.tech_challenge.adapters.driver.controller;
 
 
+import br.com.fiap.tech_challenge.adapters.driver.controller.swagger.ProdutoSwaggerInterface;
+import br.com.fiap.tech_challenge.core.domain.model.ProdutoDTO;
+import br.com.fiap.tech_challenge.core.domain.model.enums.CategoriaProduto;
+import br.com.fiap.tech_challenge.core.domain.ports.in.ProdutoServicePort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController()
 @RequestMapping("/produtos")
-public class ProdutoController {
+public class ProdutoController implements ProdutoSwaggerInterface {
+
+    private final ProdutoServicePort service;
+
+    @Autowired
+    public ProdutoController(ProdutoServicePort service) {
+        this.service = service;
+    }
+
+    @Override
+    public ResponseEntity<String> cadastrarProduto(ProdutoDTO produtoDTO) {
+        service.cadastrarProduto(produtoDTO);
+        return new ResponseEntity<>("Produto cadastrado com sucesso!", HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<Object> consultaPorCategoria(CategoriaProduto categoria) {
+        List<ProdutoDTO> produtosPorCategoria = service.buscarProdutosPorCategoria(categoria);
+        return new ResponseEntity<>(produtosPorCategoria, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<String> atualizaInformacoesProduto(ProdutoDTO produtoDTO) {
+        service.atualizarProduto(produtoDTO);
+        return new ResponseEntity<>("Produto atualizado com sucesso!", HttpStatus.ACCEPTED);
+    }
+
+    @Override
+    public ResponseEntity<String> excluirProduto(Integer id) {
+        service.excluirProduto(id);
+        return new ResponseEntity<>("Produto excluido com sucesso!", HttpStatus.OK);
+    }
 }
