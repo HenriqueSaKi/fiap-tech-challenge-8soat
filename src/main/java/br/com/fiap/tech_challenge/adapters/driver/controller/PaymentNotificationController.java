2eff6791c99b76bd83aa6a0aa9fab1a5bb54254a
felipeBarras <felipeu_santos@hotmail.com>
2024-10-03 21:10:04 -0300

    arrumando paymentnotification

--- src/main/java/br/com/fiap/tech_challenge/adapters/driver/controller/PaymentNotificationController.java
+++ src/main/java/br/com/fiap/tech_challenge/adapters/driver/controller/PaymentNotificationController.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 @RestController
 @RequestMapping("/api/payments")
 
-public class PaymentNotificationController {
+public class PaymentNotificationController {    
     public ResponseEntity<String> processPayment(@RequestBody PaymentNotification paymentNotification) {
         // atualiza o database com os dados recebidos
         System.out.println("Notificação de pagamento recebido no backend: " + paymentNotification);
